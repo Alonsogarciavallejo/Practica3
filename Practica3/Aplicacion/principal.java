96f61c1754169fb5cd195b992cecb994147c3c7f
Alonsogarciavallejo <115785376+Alonsogarciavallejo@users.noreply.github.com>
2024-04-23 14:02:01 +0200

    Update principal.java

--- Practica3/Aplicacion/principal.java
+++ Practica3/Aplicacion/principal.java
@@ -1,15 +1,19 @@
 package Aplicacion;
 
-import Matematicas.mates;
+ import Matematicas.mates;
+// importamos la clase mates para poder usar metodos de las distintas
 
 public class principal {
     public static void main(String[] args) {
+        // realizamos el main para poder inicializar la practica
         if (args.length < 1) {
             System.out.println("Por favor, proporcione el número de pasos como argumento.");
             return;
         }
+        //Se verifica si se proporciona al menos un argumento en línea de comandos. Si no se proporciona ningún argumento, se muestra un mensaje indicando que se necesita el número de pasos como argumento y la aplicación se cierra
         
         long pasos = Long.parseLong(args[0]);
+        // 
         System.out.println("El número PI es " + mates.generarNumeroPiIterativo(pasos));
     }
 }
